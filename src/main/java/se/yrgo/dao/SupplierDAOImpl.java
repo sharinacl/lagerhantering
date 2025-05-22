e0557bb9b340ccb394e1115ead8f364e7b0e40fe
GurjotKaur1501 <jotkau0115@gmail.com>
2025-05-22 07:21:32 +0200

    update supplier classes

--- src/main/java/se/yrgo/dao/SupplierDAOImpl.java
+++ src/main/java/se/yrgo/dao/SupplierDAOImpl.java
@@ -1,5 +1,48 @@
 package se.yrgo.dao;
 
-public class SupplierDAOImpl {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import se.yrgo.entity.Supplier;
 
+import java.util.List;
+
+public class SupplierDAOImpl implements SupplierDAO {
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void saveSupplier(Supplier supplier) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(supplier);
+    }
+
+    @Override
+    public Supplier getSupplierById(Long id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Supplier.class, id);
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliers() {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("FROM Supplier", Supplier.class).getResultList();
+    }
+
+    @Override
+    public void updateSupplier(Supplier supplier) {
+        Session session = sessionFactory.getCurrentSession();
+        session.update(supplier);
+    }
+
+    @Override
+    public void deleteSupplier(Long id) {
+        Session session = sessionFactory.getCurrentSession();
+        Supplier supplier = session.get(Supplier.class, id);
+        if (supplier != null) {
+            session.delete(supplier);
+        }
+    }
 }
